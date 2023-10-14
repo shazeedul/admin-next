40164cde888ef7fe1e61569dc68d20adb9cccb49
shazeedulbdtask <syedshazeedul@gmail.com>
2023-10-14 17:18:26 +0600

    dashboard component

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,4 +1,4 @@
-import { useSelector } from "react-redux";
+import Dashboard from "@/components/dashboard/index.jsx";
 
 export const metadata = {
   title: "Dashboard Page",
@@ -7,8 +7,6 @@ export const metadata = {
 
 export default function Page() {
   return (
-    <div className="flex min-h-screen p-4 bg-gray-50 dark:bg-gray-900">
-      <h1 className="text-black dark:text-white">Hello, Dashboard Page!</h1>
-    </div>
+    <Dashboard />
   );
 }
